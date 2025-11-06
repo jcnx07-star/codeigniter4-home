8fad3950bd2f5492a45dedd1b05980142a590832
Javier Cardona <jcnx07@gmail.com>
2025-11-05 20:47:13 -0600

    Proyecto básico CodeIgniter4 con controlador Home y vista funcional

---
+++ app/Views/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Vista Home</title>
+</head>
+<body>
+    <h1>¡Hola desde la vista Home!</h1>
+    <p>Proyecto CodeIgniter4 funcionando correctamente en Lubuntu.</p>
+</body>
+</html>
